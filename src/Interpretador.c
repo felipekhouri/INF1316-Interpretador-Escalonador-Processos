61880a11e720be73db8b349ffda085be160a58de
alexandrecesarba <alexandrecesarba@icloud.com>
2023-05-20 20:00:15 -0300

    comentarios adicionados

--- src/Interpretador.c
+++ src/Interpretador.c
@@ -7,100 +7,118 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <sys/types.h>
-
 #include "info.h"
 
-//Prototypes
+// Protótipo da função
 int isOK(Process *lp, int tam, int inicio, int duracao);
 
 int main(void)
 {
-	int i = 0;
-	int inicio = 0;
-	int duracao = 0;
-	char policy;
+    int i = 0; // Índice do processo
+    int inicio = 0; // Variável para armazenar o instante de início do processo
+    int duracao = 0; // Variável para armazenar a duração do processo
+    char policy; // Variável para armazenar a política do processo (I: REAL TIME, A: ROUND ROBIN)
 
-	char filename[] = "exec.txt";
-	size_t segmento;
-	Process *lstProcess;
-	char processName[10];
+    char filename[] = "exec.txt"; // Nome do arquivo de entrada
+    size_t segmento; // ID da memória compartilhada
+    Process *lstProcess; // Ponteiro para a lista de processos
+    char processName[10]; // Nome do processo
 
-	segmento = shmget(SHM_KEY, MAX_PROCESSOS * sizeof(Process), IPC_CREAT | 0666);
-	if (segmento == -1)
-	{
-		perror("Erro ao alocar memória compartilhada");
-		exit(1);
-	}
-	lstProcess = shmat(segmento, 0, 0);
+    segmento = shmget(SHM_KEY, MAX_PROCESSOS * sizeof(Process), IPC_CREAT | 0666);
+    if (segmento == -1)
+    {
+        perror("Erro ao alocar memória compartilhada");
+        exit(1);
+    }
+    lstProcess = shmat(segmento, 0, 0);
+    FILE *fp = fopen(filename, "r"); // Abre o arquivo para leitura
+	
+    if (!fp)
+    {
+        puts("Erro ao abrir o arquivo.");
+        exit(1);
+    }
 
-	FILE *fp = fopen(filename, "r"); // abre o arquivo para leitura
-	if (!fp)
-	{
-		puts("Problem opening file fp");
-		exit(1);
-	} // Trata problema ao abrir o arquivo
+    pid_t pid = fork();
+    if (pid == 0)
+    { // Processo filho
+        while (fscanf(fp, "%*s <%[^>]> %c=<%d> D=<%d>", processName, &policy, &inicio, &duracao) != EOF)
+        {
+            // Lê cada linha do arquivo
+            if (policy == 'I')
+            { // Processo REAL TIME
+                if (isOK(lstProcess, i, inicio, duracao))
+                {
+                    strcpy(lstProcess[i].name, processName);
+                    lstProcess[i].index = i;
+                    lstProcess[i].init = inicio;
+                    lstProcess[i].duration = duracao;
+                    lstProcess[i].policy = REAL_TIME;
+                    lstProcess[i].started = FALSE;
 
-	pid_t pid = fork();
-	if (pid == 0){ // processo filho
-		while (fscanf(fp, "%*s <%[^>]> %c=<%d> D=<%d>", processName, &policy, &inicio, &duracao) != EOF){ // lê cada linha do arquivo
-			if(policy == 'I'){ // REAL TIME
-				if (isOK(lstProcess, i, inicio, duracao)){
-					strcpy(lstProcess[i].name, processName);
-					lstProcess[i].index = i;
-					lstProcess[i].init = inicio;
-					lstProcess[i].duration = duracao;
-					lstProcess[i].policy = REAL_TIME;
-					lstProcess[i].started = FALSE;
+                    i++;
+                }
+                else
+                {
+                    printf("Processo: (%s) inválido. Tempo de execução excede o limite permitido.\n", processName);
+                }
 
-					i++;
-				}
-				
-				else{
-					printf("Processo: (%s) inválido. Tempo de execução excede o limite permitido.\n", processName);
-				}
-				
-				policy = 'A';
-				inicio = -1;
-				duracao = 1;
-			}
-			else{  // ROUND ROBIN
-				strcpy(lstProcess[i].name, processName);
-				lstProcess[i].index = i;
-				lstProcess[i].init = -1;
-				lstProcess[i].duration = 1;
-				lstProcess[i].policy = ROUND_ROBIN;
-				lstProcess[i].started = FALSE;
+                policy = 'A';
+                inicio = -1;
+                duracao = 1;
+            }
+            else
+            { // Processo ROUND ROBIN
+                strcpy(lstProcess[i].name, processName);
+                lstProcess[i].index = i;
+                lstProcess[i].init = -1;
+                lstProcess[i].duration = 1;
+                lstProcess[i].policy = ROUND_ROBIN;
+                lstProcess[i].started = FALSE;
+                i++;
+            }
+            sleep(1);
+        }
+    }
+    else if (pid > 0)
+    { // Processo pai
+        char *argv[] = {NULL};
+        sleep(1);
+        execvp("./escalonador", argv); // Executa o escalonador
+    }
 
-				i++;
-			}
-			sleep(1);
-		}
-	}
-
-	else if (pid > 0){ // processo pai
-		char *argv[] = {NULL};
-		sleep(1);
-		execvp("./escalonador", argv); // executa o escalonador
-	}
-
-	fclose(fp); // fecha o arquivo
-	return 0;
+    fclose(fp); // Fecha o arquivo
+    return 0;
 }
 
-int isOK(Process *lp, int tam, int inicio, int duracao){
-	for (int i = 0; i < tam; i++){
-		if ((inicio >= lp[i].init) && (inicio <= (lp[i].init + lp[i].duration))){
-			return FALSE;
-		}
+/*
+    A função "isOK" verifica se um novo processo pode ser executado no instante de início especificado.
+    Ela percorre a lista de processos já existentes e verifica se há conflito de tempo.
+    Se houver algum processo em execução no intervalo [inicio, inicio + duracao] ou se o tempo de execução ultrapassar 60 segundos,
+    retorna FALSE, indicando que o processo não pode ser executado.
+    Caso contrário, retorna TRUE, indicando que o processo pode ser executado.
+*/
+int isOK(Process *lp, int tam, int inicio, int duracao)
+{
+    for (int i = 0; i < tam; i++)
+    {
+        if ((inicio >= lp[i].init) && (inicio <= (lp[i].init + lp[i].duration)))
+        {
+            return FALSE;
+        }
 
-		if (((inicio + duracao) >= lp[i].init) && ((inicio + duracao) <= (lp[i].init + lp[i].duration))){
-			return FALSE;
-		}
-	}
+        if (((inicio + duracao) >= lp[i].init) && ((inicio + duracao) <= (lp[i].init + lp[i].duration)))
+        {
+            return FALSE;
+        }
+    }
 
-	if ((inicio + duracao) > 60){
-		return FALSE;
-	}
+    if ((inicio + duracao) > 60)
+    {
+        return FALSE;
+    }
 
-	return TRUE;
+    return TRUE;
 }
+
+
--- src/escalonador.c
+++ src/escalonador.c
@@ -11,180 +11,184 @@
 
 #include "info.h"
 
-//Protótipos:
-// void handler1(int sig);
+// Protótipos das funções
 void handler(int sig);
 char* concatenarStrings(const char* str1, const char* str2);
 void execProcess(Process currentP);
 
-//Globais:
-// int io_bound = FALSE;
+// Variáveis globais
 int termina = FALSE;
 
 int main(void){
-	int shared_memory, shmid_pid;
-	Process currentP;
-	Process *processInfo;
-	pid_t* pid;
-	
-	int i = 0;
-	
-	struct timeval init, end;
-	float sec;
+    int shared_memory, shmid_pid; // IDs da memória compartilhada
+    Process currentP; // Variável para armazenar o processo atual
+    Process *processInfo; // Ponteiro para a informação do processo
+    pid_t* pid; // Ponteiro para o ID do processo
+    
+    int i = 0; // Índice do processo
+    struct timeval init, end; // Variáveis para medir o tempo
+    float sec; // Variável para armazenar os segundos
 
-	// Anexar à memória compartilhada
-	shared_memory = shmget(SHM_KEY, MAX_PROCESSOS * sizeof(Process), IPC_CREAT | 0666);
-	processInfo = (Process *)shmat(shared_memory, 0, 0);
-	if (!processInfo){
-		perror("Erro ao anexar à memória compartilhada do processInfo.\n");
-		exit(1);
-	}
+    // Anexar à memória compartilhada
+    shared_memory = shmget(SHM_KEY, MAX_PROCESSOS * sizeof(Process), IPC_CREAT | 0666);
+    processInfo = (Process *)shmat(shared_memory, 0, 0);
+    if (!processInfo){
+        perror("Erro ao anexar à memória compartilhada do processInfo.\n");
+        exit(1);
+    }
 
-	// Anexar à memória compartilhada
-	shmid_pid = shmget(SHM_KEY2, sizeof(pid_t), IPC_CREAT | 0666);
-	pid = shmat(shmid_pid, 0, 0);
-	if (!pid){
-		perror("Erro ao anexar à memória compartilhada do pid.\n");
-		exit(1);
-	}
+    // Anexar à memória compartilhada
+    shmid_pid = shmget(SHM_KEY2, sizeof(pid_t), IPC_CREAT | 0666);
+    pid = shmat(shmid_pid, 0, 0);
+    if (!pid){
+        perror("Erro ao anexar à memória compartilhada do pid.\n");
+        exit(1);
+    }
 
-	Queue filaRR;
-	Queue filaRT;
-	Queue filaIO;
-	initQueue(&filaRR);
-	initQueue(&filaRT);
-	initQueue(&filaIO);
+    Queue filaRR; // Fila para políticas Round Robin
+    Queue filaRT; // Fila para políticas Real Time
+    Queue filaIO; // Fila para políticas I/O Bound
+    initQueue(&filaRR); // Inicializa a fila Round Robin
+    initQueue(&filaRT); // Inicializa a fila Real Time
+    initQueue(&filaIO); // Inicializa a fila I/O Bound
+    Process p; // Variável para armazenar o processo
+    signal(SIGINT, handler); // Configura o tratador de sinal para SIGINT (Ctrl+C)
+    gettimeofday(&init, NULL); // Obtém o tempo de início
 
-	Process p;
+    while (!termina){
+        gettimeofday(&end, NULL); // Obtém o tempo atual
+        sec = ((end.tv_sec - init.tv_sec) % 60); // Calcula os segundos decorridos
+        printf("\n%.1f'\n", sec);
+        
+        if (processInfo[i].index == i){
+            // Se ainda recebe processo, entra aqui
+            currentP = processInfo[i];
 
-	// signal(SIGUSR1, handler1); // recebeu sinal de IO bound
-	signal(SIGINT, handler); // Se receber interrupção do teclado (Ctrl+C), QUEBRA O LOOP INFINITO
+            if (currentP.policy == REAL_TIME){
+                enqueue(&filaRT, currentP); // Adiciona o processo na fila Real Time
+                queueSort(&filaRT); // Ordena a fila Real Time com base na prioridade
+            }
+            else if (currentP.policy == ROUND_ROBIN){
+                enqueue(&filaRR, currentP); // Adiciona o processo na fila Round Robin
+                queueSort(&filaRR); // Ordena a fila Round Robin com base na prioridade
+            }
 
-	gettimeofday(&init, NULL);
-	while (!termina){
+            i++;
+        } 
 
-		gettimeofday(&end, NULL);
-		sec = ((end.tv_sec - init.tv_sec) % 60);
-		printf("\n%.1f'\n", sec);
-		
-		if (processInfo[i].index == i){/*Se ainda recebe processo entra aqui*/ 
-			currentP = processInfo[i];
+        /* Inicia a execução dos processos */ 
+        /* Processo do Real Time */
+        if ((!isEmpty(&filaRT)) && (filaRT.front->process.init == sec)){
+            // O primeiro da fila entra em execução
+            p = filaRT.front->process;
+            if (!p.started){
+                execProcess(p); // Executa o processo pela primeira vez
+                sleep(p.duration); // Deixa o programa parado pelo tempo do processo
+                p.pid = *pid; // Pega o pid do processo
+                p.started = TRUE; // Indica que o processo começou
+            }
+            else{
+                kill(p.pid, SIGCONT); // Continua o processo já executado uma vez
+                sleep(p.duration); // Deixa o programa parado pelo tempo do processo
+            }
 
-			if (currentP.policy == REAL_TIME){
-				enqueue(&filaRT, currentP);
-				queueSort(&filaRT);
-			}
+            kill(p.pid, SIGSTOP); // Pausa o processo
+            
+            dequeue(&filaRT); // Remove o processo da fila Real Time
+            enqueue(&filaRT, p); // Adiciona o processo de volta na fila Real Time
+            printf("\n\nFila Real Time:\n");
+            displayQueue(&filaRT); // Imprime a Fila de processos Real Time
+        }
+        /* Processo do Round Robin */
+        else if (!isEmpty(&filaRR)){
+            p = filaRR.front->process;
+        
+            if (!p.started)
+			{
+                execProcess(p); // Executa o processo pela primeira vez    
+                sleep(p.duration); // Deixa o programa parado pelo tempo do processo
+                p.pid = *pid; // Pega o PID do processo
+                p.started = TRUE; // Indica que o processo começou
+            }
+            else
+			{
+                kill(p.pid, SIGCONT); // Continua o processo já executado uma vez
+                sleep(p.duration); // Deixa o programa parado pelo tempo do processo
+            }
+            kill(p.pid, SIGSTOP); // Pausa o processo         
+            dequeue(&filaRR); // Remove o processo da fila Round Robin
+            enqueue(&filaRR, p); // Adiciona o processo de volta na fila Round Robin
+            printf("\n\nFila Round Robin:\n");
+            displayQueue(&filaRR); // Imprime a Fila de processos Round Robin
+        }
+    }
 
-			else if (currentP.policy == ROUND_ROBIN){
-				enqueue(&filaRR, currentP);
-				queueSort(&filaRR);
-			}
-
-			i++;
-		} 
-
-
-	/*Inicia a execução dos processos*/ 
-		/* Processo do Real Time */
-		if ((!isEmpty(&filaRT)) && (filaRT.front->process.init == sec)) {  // Primeiro da fila entra em execução
-		   
-			p = filaRT.front->process;
-			if (!p.started){
-				execProcess(p); 		// Executa processo pela primeira vez
-				sleep(p.duration);		// deixa o programa parado pelo tempo do processo
-				p.pid = *pid;			// pega o pid do processo
-				p.started = TRUE;		// diz que o processo começou
-			}
-			else{
-				kill(p.pid, SIGCONT);	// Continua o processo já executado uma vez
-				sleep(p.duration);		// deixa o programa parado pelo tempo do processo
-			}
-
-			kill(p.pid, SIGSTOP);
-			
-			dequeue(&filaRT);
-			enqueue(&filaRT, p);
-			printf("\n\nFila Real Time:\n");
-			displayQueue(&filaRT); //Imprime Fila de processos Real Time
-
-		}
-		/* Processo do Round Robin */
-		else if (!isEmpty(&filaRR)){
-			p = filaRR.front->process;
-			
-			if (!p.started){
-				execProcess(p);  		// Executa processo pela primeira vez	
-				sleep(p.duration);		// deixa o programa parado pelo tempo do processo
-				p.pid = *pid;     		// pega o PID do processo
-				p.started = TRUE;		// diz que o processo começou
-			}
-			else{
-				kill(p.pid, SIGCONT);	// Continua o processo já executado uma vez
-				sleep(p.duration);		// deixa o programa parado pelo tempo do processo
-			}
-
-			kill(p.pid, SIGSTOP);
-						
-			dequeue(&filaRR);
-            /*if(io_bound == TRUE){
-                if(fork() == 0){
-                    enqueue(&filaIO, p);
-					printf("Processo = %s -- PID = %d -- Entrou IO\n", p.name, p.pid);
-                    sleep(3);
-                    dequeue(&filaIO);
-                    io_bound = FALSE;
-                }
-            }*/
-			enqueue(&filaRR, p);
-			printf("\n\nFila Round Robin:\n");
-			displayQueue(&filaRR); //Imprime Fila de processos Round Robin
-		}
-	}
-
-	/* Libera a memória compartilhada */ 
-	shmctl(shared_memory, IPC_RMID, 0);
-	shmctl(shmid_pid, IPC_RMID, 0);
-	
-	return 0;
+    /* Libera a memória compartilhada */ 
+    shmctl(shared_memory, IPC_RMID, 0);
+    shmctl(shmid_pid, IPC_RMID, 0);
+    
+    return 0;
 }
 
+
+
+/*
+    A função "handler" é um tratador de sinal para o sinal SIGINT (Ctrl+C).
+    Quando esse sinal é recebido, a variável global "termina" é configurada para TRUE,
+    indicando que o programa deve terminar.
+*/
 void handler(int sig) {
-	termina = TRUE;
+    termina = TRUE;
 }
 
-/*void handler1(int sig) {
-	io_bound = TRUE;
-}*/
 
+
+/*
+    A função "concatenarStrings" recebe duas strings e retorna uma nova string
+    que é a concatenação das duas.
+    Ela aloca memória suficiente para armazenar a nova string e a concatena
+    usando as funções strcpy e strcat.
+    Caso ocorra algum erro na alocação de memória, a função exibe uma mensagem de erro e encerra o programa.
+*/
 char* concatenarStrings(const char* str1, const char* str2) {
-	size_t tamanhoStr1 = strlen(str1);
-	size_t tamanhoStr2 = strlen(str2);
-	size_t tamanhoTotal = tamanhoStr1 + tamanhoStr2 + 1;
+    size_t tamanhoStr1 = strlen(str1);
+    size_t tamanhoStr2 = strlen(str2);
+    size_t tamanhoTotal = tamanhoStr1 + tamanhoStr2 + 1;
 
-	char* resultado = (char*)malloc(tamanhoTotal);
+    char* resultado = (char*)malloc(tamanhoTotal);
 
-	if (resultado == NULL) {
-		perror("Erro ao alocar memória");
-		exit(1);
-	}
+    if (resultado == NULL) {
+        perror("Erro ao alocar memória");
+        exit(1);
+    }
 
-	strcpy(resultado, str1);
-	strcat(resultado, str2);
+    strcpy(resultado, str1);
+    strcat(resultado, str2);
 
-	return resultado;
+    return resultado;
 }
 
+/*
+    A função "execProcess" recebe um objeto Processo e executa o programa associado a ele.
+    Ela cria um caminho completo para o programa, concatenando o diretório "./programas/" com o nome do programa.
+    Em seguida, cria um array de argumentos vazio e verifica se é um novo processo (filho) usando a função fork().
+    Se for um novo processo, ele substitui a imagem atual pelo programa usando execvp(),
+    passando o caminho e o array de argumentos vazio.
+    Após a execução do programa, a função retorna.
+*/
 void execProcess(Process p){
-	char inicioPath[] = "./programas/";
-	char *path;
+    char inicioPath[] = "./programas/";
+    char *path;
 
-	path = concatenarStrings(inicioPath, p.name);
-	
-	char *argv[] = {NULL};
-	
-	if(fork() == 0){
-		printf("Iniciando o programa %s\n", path);
-		execvp(path, argv);
-	} 
-	return;
+    path = concatenarStrings(inicioPath, p.name);
+    
+    char *argv[] = {NULL};
+    
+    if(fork() == 0){
+        printf("Iniciando o programa %s\n", path);
+        execvp(path, argv);
+    } 
+    return;
 }
+
+
